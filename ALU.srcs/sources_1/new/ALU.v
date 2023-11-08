122bf4d09abf2f76c797847b07e163c55fda44cf
Franco Nicolas Bottini <101523922+FrancoNB@users.noreply.github.com>
2023-11-08 15:17:19 -0300

    Update ALU.v

--- ALU.srcs/sources_1/new/ALU.v
+++ ALU.srcs/sources_1/new/ALU.v
@@ -1,26 +1,26 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Company: Facultad de Ciencias Exactas Físicas y Naturales
+// Company: Facultad de Ciencias Exactas FÃ­sicas y Naturales
 // Authors: Robledo Valentin y Bottini Franco Nicolas 
 // 
 // Module Name: ALU
-// Project Name: TP 1 - Arquitectura de Computadoras 2023 - Unidad Aritmetico Logica
-// Description: Implementación de una unidad aritmetico logica con las operaciones ADD, SUB, AND, OR, XOR, SRA, SRL, NOR, RESET
+// Project Name: TP 2 - Arquitectura de Computadoras 2023 - UART
+// Description: ImplementaciÃ³n de una unidad aritmetico logica con las operaciones ADD, SUB, AND, OR, XOR, SRA, SRL, NOR, RESET
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 1ps
 
-`include "ALU.vh"
+`include "alu.vh"
 
-module ALU
+module alu
     #(
-        parameter IO_BUS_WIDTH = `IO_BUS_WIDTH
+        parameter IO_BUS_WIDTH  = `IO_BUS_WIDTH,
+        parameter OP_CODE_WIDTH = `OP_CODE_WIDTH
     )
     (
-        input  wire [`OP_CODE_WIDTH - 1 : 0] op_code, 
-        input  wire [IO_BUS_WIDTH - 1 : 0]   data_a,
-        input  wire [IO_BUS_WIDTH - 1 : 0]   data_b,
-        
-        output wire [IO_BUS_WIDTH - 1 : 0]   out_data
+        input         [OP_CODE_WIDTH - 1 : 0] op_code, 
+        input  signed [IO_BUS_WIDTH - 1 : 0]  data_a,
+        input  signed [IO_BUS_WIDTH - 1 : 0]  data_b,
+        output signed [IO_BUS_WIDTH - 1 : 0]  out_data
     );
     
     reg[IO_BUS_WIDTH - 1 : 0] result;
@@ -32,9 +32,9 @@ module ALU
             `OP_SUB  : result <= data_a - data_b;
             `OP_AND  : result <= data_a & data_b;
             `OP_OR   : result <= data_a | data_b;
-            `OP_XOR  : result <= data_a ^ data_b;
-            `OP_SRA  : result <= data_a >> data_b;
-            `OP_SRL  : result <= data_a >>> data_b;
+            `OP_XOR  : result <= data_a ^  data_b;
+            `OP_SRA  : result <= data_a >>> data_b;
+            `OP_SRL  : result <= data_a >> data_b;
             `OP_NOR  : result <= ~(data_a | data_b);
             default  : result <= {IO_BUS_WIDTH {1'bz}};
         endcase
